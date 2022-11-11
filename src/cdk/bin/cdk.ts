e6fab2a66d28921f51bb687ca2e18a272188b4cd
Felix <23635466+its-felix@users.noreply.github.com>
2022-11-11 01:20:56 +0100

    misc: move CDK to submodule

--- src/cdk/bin/cdk.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import {BackgroundJobsStack} from '../lib/background-jobs-stack';
-
-const app = new cdk.App();
-
-new BackgroundJobsStack(app, 'Gw2AuthBackgroundJobs-Beta', {});
-new BackgroundJobsStack(app, 'Gw2AuthBackgroundJobs-Prod', {});
